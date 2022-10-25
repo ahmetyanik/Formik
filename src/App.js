c8975aa643c77cde88c3b7cd364c49d273899ac7
ahmetyanik <ahmetyank4242@gmail.com>
2022-10-25 20:57:29 +0200

    formik form is ready to use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="brand-box">
+        <h1>Magic Box</h1>
+        <p>Build forms in React without the tears.</p>
+      </div>
+      <div className="magic-form">
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            agree: false,
+            favoriteColor: "",
+          }}
+          validationSchema={Yup.object({
+            name: Yup.string().required("Isim bos birakilamaz."),
+            email: Yup.string()
+              .email()
+              .required("Email alani bos birakilamaz!"),
+            agree: Yup.boolean().required("Kosullari kabul etmelisin!"),
+            favoriteColor: Yup.string()
+              .required("Renk alani bos birakilamaz!")
+              .oneOf(["red", "blue", "green"]),
+          })}
+          onSubmit={(values, { resetForm, setSubmitting }) => {
+            console.log(values);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {({
+            values,
+            errors,
+            handleChange,
+            handleSubmit,
+            handleReset,
+            dirty,
+            touched,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <h3>Kaydol</h3>
+              <label htmlFor="name">isim</label>
+              <input
+                id="name"
+                type="text"
+                placeholder="ahmet"
+                className="input"
+                onChange={handleChange}
+                value={values.name}
+              />
+              {errors.name && touched.name && (
+                <div className="input-feedback">{errors.name}</div>
+              )}
+              <label htmlFor="email" className="topMargin">
+                E-mail
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder="ahmetyanikdev@gmail.com"
+                className="input"
+                onChange={handleChange}
+                value={values.email}
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
+              <label htmlFor="favoriteColor" className="topMargin">
+                Favori Renk
+              </label>
+              <div>
+                <select
+                  id="favoriteColor"
+                  value={values.favoriteColor}
+                  onChange={handleChange}
+                  style={{
+                    marginTop: 10,
+                    width: "150px",
+                    padding: "10px 15px",
+                    outline: "none",
+                  }}
+                >
+                  <option value="" label="Renk Sec" />
+                  <option value="red" label="Kirmizi" />
+                  <option value="blue" label="Mavi" />
+                  <option value="green" label="Yesil" />
+                </select>
+              </div>
+              {errors.favoriteColor && touched.favoriteColor && (
+                <div className="input-feedback">{errors.favoriteColor}</div>
+              )}
+              <div className="checkbox topMargin">
+                <input
+                  id="agree"
+                  type="checkbox"
+                  value={values.agree}
+                  onChange={handleChange}
+                />
+                <label htmlFor="agree" className="checkbox-label">
+                  Sözlesmeyi okudum kabul ediyorum.
+                </label>
+              </div>
+              <button type="submit" disabled={!dirty || isSubmitting}>
+                Kaydol
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
